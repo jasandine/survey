1ad673ee0ad063e0f6fd1691c304e35531732734
Clem freeman and Joey Sandine <clemf@gmail.com>
2015-01-28 11:49:16 -0800

    Add survey update and delete functions

--- app.rb
+++ app.rb
@@ -8,15 +8,36 @@ get '/' do
 end
 
 get '/questions/:id' do
+  @question = Question.find(params.fetch("id"))
   erb :question
 end
 
 get '/surveys/:id' do
+  @survey = Survey.find(params.fetch("id"))
   erb :survey
 end
 
 post '/add_survey' do
   title = params.fetch("title")
   Survey.create(title: title)
+  redirect back
+end
+
+post '/add_question' do
+  question = params.fetch("question")
+  survey_id = params.fetch("survey_id")
+  Question.create(question: question, survey_id: survey_id)
+  redirect back
+end
+
+delete '/surveys/:id' do
+  Survey.find(params.fetch("id")).destroy
+  redirect '/'
+end
+
+patch '/surveys/:id' do
+  @survey = Survey.find(params.fetch("id"))
+  title = params.fetch("title")
+  @survey.update(title: title)
   redirect back
 end
